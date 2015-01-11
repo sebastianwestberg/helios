e5c8470db58a4e8c99e057d2637ef5b4da8ea156
Sebastian Westberg <sebastian@stormfors.se>
2015-01-11 18:11:39 +0100

    Spaces replaces tabs for indention.

--- src/Helios/Container.php
+++ src/Helios/Container.php
@@ -4,40 +4,40 @@ namespace Helios;
 
 class Container implements \ArrayAccess
 {
-	private $storage = array();
+    private $storage = array();
 
-	public function __construct()
-	{
-		// no magic for now
-	}
+    public function __construct()
+    {
+        // no magic for now
+    }
 
-	public function offsetExists($offset)
-	{
-		return isset($this->storage[$offset]);
-	}
+    public function offsetExists($offset)
+    {
+        return isset($this->storage[$offset]);
+    }
 
-	public function offsetGet($offset)
-	{
-		if (!isset($this->storage[$offset])) {
-			throw new \InvalidArgumentException(sprintf("'%s' is not recognized by the container", $offset));
-		}
+    public function offsetGet($offset)
+    {
+        if (!isset($this->storage[$offset])) {
+            throw new \InvalidArgumentException(sprintf("'%s' is not recognized by the container", $offset));
+        }
 
-		if (is_object($storageEntry = $this->storage[$offset])) {
-			return $storageEntry($this);
-		} else {
-			return $storageEntry;
-		}
-	}
+        if (is_object($storageEntry = $this->storage[$offset])) {
+            return $storageEntry($this);
+        } else {
+            return $storageEntry;
+        }
+    }
 
-	public function offsetSet($offset, $value)
-	{
-		$this->storage[$offset] = $value;
-	}
+    public function offsetSet($offset, $value)
+    {
+        $this->storage[$offset] = $value;
+    }
 
-	public function offsetUnset($offset)
-	{
-		if (isset($this->storage[$offset])) {
-			unset($this->storage[$offset]);
-		}
-	}
+    public function offsetUnset($offset)
+    {
+        if (isset($this->storage[$offset])) {
+            unset($this->storage[$offset]);
+        }
+    }
 }
